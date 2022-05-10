7b277c0a96f7f401d9e6279744dae716ea92404f
Genesis Agravio <62141010+agraviog@users.noreply.github.com>
2022-05-10 17:30:15 +0800

    opac 0

--- widget.js
+++ widget.js
@@ -267,7 +267,7 @@ Webflow.push(function () {
   }
 
   async function startRecordProcess() {
-    $(".headphone-on--vm-widget").css("opacity", "0");
+    $(".headphone-on--vm-widget").css({ display: "none" });
     resetPlay();
     if (navigator.mediaDevices) {
       await validateMicrophoneAccess();
