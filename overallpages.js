a8a16ef103fd8055fa1736945d434bddd2b260e4
Genesis Agravio <agraviog@gmail.com>
2022-06-13 04:13:55 +0800

    animate on scroll

--- overallpages.js
+++ overallpages.js
@@ -1,7 +1,3 @@
-// Copyright start
-// © Code by T.RICKS, https://www.tricksdesign.com/
-// You have the license to use this code in your projects but not redistribute it to others
-
 // Find all text with .tricks class and break each letter into a span
 var tricksWord = document.getElementsByClassName("tricks");
 for (var i = 0; i < tricksWord.length; i++) {
@@ -20,52 +16,6 @@ for (var i = 0; i < tricksLetter.length; i++) {
     "<span class='letter'>$&</span>"
   );
 }
-// Copyright end
-
-// Slide In Animation
-var slideIn = anime.timeline({
-  loop: false,
-  autoplay: false,
-});
-
-slideIn
-  .add({
-    targets: ".slide-in .letter",
-    opacity: [0, 1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i + 1),
-  })
-  .add({
-    targets: ".slide-in",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
-
-// Slide Up Animation
-var slideUp = anime.timeline({
-  loop: false,
-  autoplay: false,
-});
-
-slideUp
-  .add({
-    targets: ".slide-up .letter",
-    translateY: ["1.1em", 0],
-    opacity: [0, 1],
-    translateZ: 0,
-    duration: 750,
-    delay: (el, i) => 50 * i,
-  })
-  .add({
-    targets: ".slide-up",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
 
 // Fade Up Animation
 var fadeUp = anime.timeline({
@@ -83,59 +33,8 @@ fadeUp.add({
   delay: (el, i) => 300 + 30 * i,
 });
 
-// Rotate In Animation
-var rotateIn = anime.timeline({
-  loop: false,
-  autoplay: false,
-});
-
-rotateIn
-  .add({
-    targets: ".rotate-in .letter",
-    translateY: ["1.1em", 0],
-    translateX: ["0.55em", 0],
-    translateZ: 0,
-    rotateZ: [180, 0],
-    duration: 750,
-    easing: "easeOutExpo",
-    delay: (el, i) => 50 * i,
-  })
-  .add({
-    targets: ".rotate-in",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
-
-// Pop In Animation
-var popIn = anime.timeline({
-  loop: false,
-  autoplay: false,
-});
-
-popIn
-  .add({
-    targets: ".pop-in .letter",
-    scale: [0, 1],
-    duration: 1500,
-    elasticity: 600,
-    delay: (el, i) => 45 * (i + 1),
-  })
-  .add({
-    targets: ".pop-in",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
-
 // Play your animation with these
 fadeUp.play();
-slideUp.play();
-slideIn.play();
-rotateIn.play();
-popIn.play();
 
 // Wait before playing animation
 setTimeout(() => {
@@ -155,7 +54,7 @@ $(".your-button-class").mouseenter(function () {
 // Play animation when scrolled into view
 $("#heading-container").on("inview", function (event, isInView) {
   if (isInView) {
-    // Put the play below this line
+    fadeUp.play();
   } else {
   }
 });
