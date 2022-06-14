ee815e60c9fa809ffe4c68dea5a929ec12a34cb4
Genesis Agravio <agraviog@gmail.com>
2022-06-14 14:52:40 +0800

    scripts

--- overallpages.js
+++ overallpages.js
@@ -1,101 +1,137 @@
-// Find all text with .tricks class and break each letter into a span
-var tricksWord = document.getElementsByClassName("tricks");
-for (var i = 0; i < tricksWord.length; i++) {
-  var wordWrap = tricksWord.item(i);
-  wordWrap.innerHTML = wordWrap.innerHTML.replace(
-    /(^|<\/?[^>]+>|\s+)([^\s<]+)/g,
-    '$1<span class="tricksword">$2</span>'
-  );
-}
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  //voices
+  var plyHarmony = document.getElementById("harmony-vm");
+  var plyHarmonyOrig = document.getElementById("harmony");
 
-var tricksLetter = document.getElementsByClassName("tricksword");
-for (var i = 0; i < tricksLetter.length; i++) {
-  var letterWrap = tricksLetter.item(i);
-  letterWrap.innerHTML = letterWrap.textContent.replace(
-    /\S/g,
-    "<span class='letter'>$&</span>"
-  );
-}
+  //checkbox, play
+  var togglebtn = document.getElementById("checkbox_nplayer");
+  var progressBar = document.getElementById("progress-play");
 
-// Fade Up Animation
-var fadeUp = anime.timeline({
-  loop: false,
-  autoplay: false,
-});
+  var playing = true;
+  var track1 = plyHarmony;
+  var track2 = plyHarmonyOrig;
+  var currentTime;
+  var musicDuration;
+  var progress;
 
-fadeUp.add({
-  targets: ".fade-up .letter",
-  translateY: [100, 0],
-  translateZ: 0,
-  opacity: [0, 1],
-  easing: "easeOutExpo",
-  duration: 1400,
-  delay: (el, i) => 300 + 30 * i,
-});
+  //on load
+  $(window).on("load", function () {
+    $("#nplayer_play").removeClass("control_disable disable-play");
+    $(".checkbox--nplayer").addClass("w--redirected-checked");
+    togglebtn.disabled = true;
+  });
 
-// Play your animation with these
-fadeUp.play();
-
-// Wait before playing animation
-setTimeout(() => {
-  // Put the play below this line
-}, 800);
-
-// Play animaton when something is clicked
-$(".your-button-class").click(function () {
-  // Put the play below this line
-});
-
-// Play animaton when hovered in
-$(".your-button-class").mouseenter(function () {
-  // Put the play below this line
-});
-
-// Play animation when scrolled into view
-$("#text-container").on("inview", function (event, isInView) {
-  if (isInView) {
-  } else {
+  function togglePlay(showPlay) {
+    if (showPlay) {
+      document.getElementsByClassName("play_icon--nplayer")[0].style.display =
+        "block";
+      document.getElementsByClassName("pause_icon--nplayer")[0].style.display =
+        "none";
+    } else {
+      document.getElementsByClassName("play_icon--nplayer")[0].style.display =
+        "none";
+      document.getElementsByClassName("pause_icon--nplayer")[0].style.display =
+        "block";
+    }
   }
-});
 
-$(".link__wrap").on("mouseenter", function () {
-  $(".link__circle").addClass("btn__hover-in");
-});
-$(".link__wrap").on("mouseleave", function () {
-  $(".link__circle").removeClass("btn__hover-in");
-});
+  plyHarmony.addEventListener("timeupdate", (e) => {
+    currentTime = e.target.currentTime.toFixed(2);
+    musicDuration = plyHarmony.duration.toFixed(2);
+    progress = (currentTime / musicDuration) * 100;
+    progressBar.style.width = `${progress}%`;
 
-$(".nav__btn").on("mouseenter", function () {
-  $(".link__circle--nav").addClass("btn__hover-in--nav");
-});
-$(".nav__btn").on("mouseleave", function () {
-  $(".link__circle--nav").removeClass("btn__hover-in--nav");
-});
+    if (currentTime == musicDuration) {
+      togglePlay(true);
+      togglebtn.disabled = true;
+      $(".checkbox--nplayer").addClass("w--redirected-checked");
+      playing = true;
+      track1.muted = false;
+    }
+  });
 
-$(".link-tw").on("mouseenter", function () {
-  $("#circle-tw").addClass("btn__hover-in--footer");
-});
-$(".link-tw").on("mouseleave", function () {
-  $("#circle-tw").removeClass("btn__hover-in--footer");
-});
+  $(".control_play--nplayer").on("click", function (e) {
+    togglebtn.disabled = false;
 
-$(".link-tt").on("mouseenter", function () {
-  $("#circle-tt").addClass("btn__hover-in--footer");
-});
-$(".link-tt").on("mouseleave", function () {
-  $("#circle-tt").removeClass("btn__hover-in--footer");
-});
+    if (playing) {
+      playing = !playing;
+      if (track1.muted) {
+        track2.play();
+        track1.play();
+        track1.muted = true;
+      } else {
+        track1.play();
+        track2.play();
+        track2.muted = true;
+      }
+      togglePlay(false);
+    } else {
+      playing = !playing;
+      if (track1.muted) {
+        track2.pause();
+      }
+      track1.pause();
+      togglePlay(true);
+      togglebtn.disabled = true;
+    }
+  });
 
-$(".link-yt").on("mouseenter", function () {
-  $("#circle-yt").addClass("btn__hover-in--footer");
-});
-$(".link-yt").on("mouseleave", function () {
-  $("#circle-yt").removeClass("btn__hover-in--footer");
-});
+  $("#checkbox_nplayer").on("click", (e) => {
+    if (track2.muted) {
+      track1.muted = true;
+      track2.currentTime = track1.currentTime;
+      track2.muted = false;
+      playing = false;
+      console.log("Track 2");
+    } else {
+      track2.muted = true;
+      track1.currentTime = track2.currentTime;
+      track1.muted = false;
+      playing = false;
+      console.log("Track 1");
+    }
+  });
 
-$(".link-dc").on("mouseenter", function () {
-  $("#circle-dc").addClass("btn__hover-in--footer");
-});
-$(".link-dc").on("mouseleave", function () {
-  $("#circle-dc").removeClass("btn__hover-in--footer");
+  $(".link__wrap").on("mouseenter", function () {
+    $(".link__circle").addClass("btn__hover-in");
+  });
+  $(".link__wrap").on("mouseleave", function () {
+    $(".link__circle").removeClass("btn__hover-in");
+  });
+
+  $(".nav__btn").on("mouseenter", function () {
+    $(".link__circle--nav").addClass("btn__hover-in--nav");
+  });
+  $(".nav__btn").on("mouseleave", function () {
+    $(".link__circle--nav").removeClass("btn__hover-in--nav");
+  });
+
+  $(".link-tw").on("mouseenter", function () {
+    $("#circle-tw").addClass("btn__hover-in--footer");
+  });
+  $(".link-tw").on("mouseleave", function () {
+    $("#circle-tw").removeClass("btn__hover-in--footer");
+  });
+
+  $(".link-tt").on("mouseenter", function () {
+    $("#circle-tt").addClass("btn__hover-in--footer");
+  });
+  $(".link-tt").on("mouseleave", function () {
+    $("#circle-tt").removeClass("btn__hover-in--footer");
+  });
+
+  $(".link-yt").on("mouseenter", function () {
+    $("#circle-yt").addClass("btn__hover-in--footer");
+  });
+  $(".link-yt").on("mouseleave", function () {
+    $("#circle-yt").removeClass("btn__hover-in--footer");
+  });
+
+  $(".link-dc").on("mouseenter", function () {
+    $("#circle-dc").addClass("btn__hover-in--footer");
+  });
+  $(".link-dc").on("mouseleave", function () {
+    $("#circle-dc").removeClass("btn__hover-in--footer");
+  });
 });
