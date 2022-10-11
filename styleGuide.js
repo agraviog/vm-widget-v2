f3451fd5b9f9b6f874f2facdb7c5a78ada406c98
agraviog <agraviog@gmail.com>
2022-10-11 14:52:26 +0800

    page-partners script

--- styleGuide.js
+++ styleGuide.js
@@ -1,15 +1,18 @@
-// Nav menu mobile open
-$(".nav-menu").click(function (e) {
-  e.preventDefault();
-  $(".nav-mobile").addClass("nav-mobile-open");
-  // Prevent body from scrolling
-  $("body").css("overflow", "hidden");
-});
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  // Nav menu mobile open
+  $(".nav-menu").click(function (e) {
+    e.preventDefault();
+    $(".nav-mobile").addClass("nav-mobile-open");
+    // Prevent body from scrolling
+    $("body").css("overflow", "hidden");
+  });
 
-// Nav menu mobile close
-$(".close-nav-button, .nav-close-wrapper").click(function (e) {
-  e.preventDefault();
-  $(".nav-mobile").removeClass("nav-mobile-open");
-  // Revert body from scrolling
-  $("body").css("overflow", "auto");
+  // Nav menu mobile close
+  $(".close-nav-button, .nav-close-wrapper").click(function (e) {
+    e.preventDefault();
+    $(".nav-mobile").removeClass("nav-mobile-open");
+    // Allow body from scrolling
+    $("body").css("overflow", "auto");
+  });
 });
